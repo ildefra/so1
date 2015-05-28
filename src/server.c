92007846c6ae687dbf4a17a1f2b3d5e57e119143
ildefra <defranceschi.java@gmail.com>
2015-05-28 19:16:27 +0200

    socket binding

--- src/server.c
+++ src/server.c
@@ -1,8 +1,21 @@
 #include <sys/socket.h>
 
+#define LISTEN_PORT 2015
+
 /* The server */
 int main(int argc, char **argv) {
 	int ds_sock;
+	struct sockaddr_in my_addr;
+	
 	ds_sock = socket(AF_INET, SOCK_STREAM, 0);
-	return ds_sock == -1 ? 1 : 0;
+	
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = LISTEN_PORT;
+	my_addr.sin_addr.s_addr = INADDR_ANY;
+	
+	bind(ds_sock, &my_addr, sizeof(my_addr));
+	
+	//do something
+	
+	close(ds_sock);
 }
