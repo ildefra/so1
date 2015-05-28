3084562236c3bd578b8e159658d13dfe18676b85
ildefra <defranceschi.java@gmail.com>
2015-05-29 01:46:37 +0200

    server now really listens + client stub

--- src/client.c
+++ src/client.c
@@ -1,8 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-/* The client */
+#define MY_PORT 2015
+
+/*
+includes usage:
+<stdio.h>   : error logging only
+<stdlib.h>  : exit statuses only
+<sys/types.h>:man says some OS could require it
+<unistd.h>  : closing sockets only
+*/
+
+/* TODO: split more */
+/* the client */
 int main(int argc, char **argv) {
 	int ds_sock;
-	ds_sock = socket(AF_INET, SOCK_STREAM, 0);
-	return ds_sock == -1 ? 1 : 0;
+    struct sockaddr_in my_addr;
+    int make_tcp_socket();
+    void close_sock(int);
+    
+    ds_sock = make_tcp_socket();
+    
+	my_addr.sin_family      = AF_INET;
+	my_addr.sin_port        = htons(MY_PORT);
+    my_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    
+    /* TODO: connect to server and do stuff */
+    
+	close_sock(ds_sock);
+}
+
+/* TODO: code clone in server.c */
+/* creates a TCP socket */
+int make_tcp_socket() {
+    int ds_sock;
+    
+    ds_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (ds_sock < 0) {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+    return ds_sock;
+}
+
+/* TODO: code clone in server.c */
+void close_sock(const int ds_sock) {
+    int close_result;
+    
+    close_result = close(ds_sock);
+	if (close_result < 0) {
+        fprintf(stderr, "could not close socket: exiting");
+        exit(EXIT_FAILURE);
+    }
 }
--- src/server.c
+++ src/server.c
@@ -2,74 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#define LISTEN_PORT 2015
+#define MY_PORT 2015
+#define LISTEN_BACKLOG 10
 
 /*
-include usage:
-<stdio.h>   : error logging
-<stdlib.h>  : exit status
-<unistd.h>  : closing sockets
+includes usage:
+<stdio.h>   : error logging only
+<stdlib.h>  : exit statuses only
+<sys/types.h>:man says some OS could require it
+<unistd.h>  : closing sockets only
 */
 
-/* TODO: split more */
+void close_sock(int);
+
 /* the server */
 int main(int argc, char **argv) {
-	int ds_sock, ds_sock_acc;
-    struct sockaddr addr;
-	int addrlen;
-    int make_tcp_socket(u_short);
-    void close_sock(int);
+	int ds_sock;
+    int make_tcp_socket();
+    void bind_to_port(int, u_short);
+    void run_server(int);
     
-    ds_sock = make_tcp_socket(LISTEN_PORT);
-    ds_sock_acc = accept(ds_sock, &addr, &addrlen);
-	if (ds_sock_acc < 0) {
-        fprintf(stderr, "could not accept incoming connections: exiting");
-        exit(EXIT_FAILURE);
-    }
-    
-	/* do something */
-	
-    close_sock(ds_sock_acc);
+    ds_sock = make_tcp_socket();
+    bind_to_port(ds_sock, MY_PORT);
+    run_server(ds_sock);
 	close_sock(ds_sock);
     return EXIT_SUCCESS;
 }
 
-/* TODO: sin_port is passed around too much */
-
-/* creates a TCP socket that listens to all addresses */
-int make_tcp_socket(const u_short sin_port) {
-    int ds_sock;
-    void safe_bind(int, u_short);
-    
-    ds_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (ds_sock < 0) {
-        fprintf(stderr, "could not create socket: exiting");
-        exit(EXIT_FAILURE);
-    }
-    
-    safe_bind(ds_sock, sin_port);
-    return ds_sock;
-}
-
 /* binds the given socket to the given port */
-void safe_bind(const int ds_sock, const u_short sin_port) {
+void bind_to_port(const int ds_sock, const u_short sin_port) {
     int bind_result;
     struct sockaddr_in my_addr;
-    struct sockaddr_in make_sockaddr(u_short);
+    struct sockaddr_in make_socket_address(u_short);
     
-	my_addr = make_sockaddr(sin_port);
+	my_addr = make_socket_address(sin_port);
 	
 	bind_result = bind(ds_sock, (struct sockaddr *) &my_addr, sizeof(my_addr));
 	if (bind_result < 0) {
-        fprintf(stderr, "could not bind socket to port %d: exiting", sin_port);
+        perror("bind()");
         exit(EXIT_FAILURE);
     }
+    printf("server listening on port %d\n", sin_port);
 }
 
 /* creates an internet address structure for listening sockets to bind to */
-struct sockaddr_in make_sockaddr(const u_short sin_port) {
+struct sockaddr_in make_socket_address(const u_short sin_port) {
     struct sockaddr_in my_addr;
     
     my_addr.sin_family      = AF_INET;
@@ -80,12 +60,44 @@ struct sockaddr_in make_sockaddr(const u_short sin_port) {
 }
 
 
+void run_server(const int ds_sock) {
+    int ds_sock_acc;
+    struct sockaddr addr;
+	int addrlen;
+    
+    listen(ds_sock, LISTEN_BACKLOG);
+    ds_sock_acc = accept(ds_sock, &addr, &addrlen);
+	if (ds_sock_acc < 0) {
+        perror("accept()");
+        exit(EXIT_FAILURE);
+    }
+    
+	/* do something */
+	
+    close_sock(ds_sock_acc);
+}
+
+
+/* TODO: code clone in client.c */
+/* creates a TCP socket */
+int make_tcp_socket() {
+    int ds_sock;
+    
+    ds_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (ds_sock < 0) {
+        perror("socket()");
+        exit(EXIT_FAILURE);
+    }
+    return ds_sock;
+}
+
+/* TODO: code clone in client.c */
 void close_sock(const int ds_sock) {
     int close_result;
     
     close_result = close(ds_sock);
 	if (close_result < 0) {
-        fprintf(stderr, "could not close socket: exiting");
+        perror("close()");
         exit(EXIT_FAILURE);
     }
 }
