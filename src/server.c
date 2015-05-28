90ec24b73c748b533370bd39986944b4afcd9e6f
ildefra <defranceschi.java@gmail.com>
2015-05-28 19:52:31 +0200

    solving compiler warnings

--- src/server.c
+++ src/server.c
@@ -1,7 +1,10 @@
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
 
 #define LISTEN_PORT 2015
 
+/* XXX: this is already too long */
 /* The server */
 int main(int argc, char **argv) {
 	int ds_sock;
@@ -9,13 +12,14 @@ int main(int argc, char **argv) {
 	
 	ds_sock = socket(AF_INET, SOCK_STREAM, 0);
 	
-	my_addr.sin_family = AF_INET;
-	my_addr.sin_port = LISTEN_PORT;
+	my_addr.sin_family      = AF_INET;
+	my_addr.sin_port        = LISTEN_PORT;
 	my_addr.sin_addr.s_addr = INADDR_ANY;
 	
-	bind(ds_sock, &my_addr, sizeof(my_addr));
+	bind(ds_sock, (struct sockaddr *) &my_addr, sizeof(my_addr));
 	
 	//do something
 	
-	close(ds_sock);
+	/* close(ds_sock); */ /* still need the header for this */
+    return EXIT_SUCCESS;
 }
