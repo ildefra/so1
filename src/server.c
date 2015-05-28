ffdb5f9ffc02b8a56427374387061a5003062c45
ildefra <defranceschi.java@gmail.com>
2015-05-28 22:54:22 +0200

    split, some error handling

--- src/server.c
+++ src/server.c
@@ -1,31 +1,54 @@
-#include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
 #include <unistd.h>
 
 #define LISTEN_PORT 2015
 
-/* XXX: this is already too long */
-/* The server */
+/*
+include usage:
+<stdio.h>   : error logging
+<stdlib.h>  : exit status
+<unistd.h>  : closing sockets
+*/
+
+/* the server */
 int main(int argc, char **argv) {
 	int ds_sock, ds_sock_acc;
-	struct sockaddr_in my_addr;
     struct sockaddr addr;
 	int addrlen;
+    int make_tcp_socket(u_short);
     
-	ds_sock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	my_addr.sin_family      = AF_INET;
-	my_addr.sin_port        = LISTEN_PORT;
-	my_addr.sin_addr.s_addr = INADDR_ANY;
-	
-	bind(ds_sock, (struct sockaddr *) &my_addr, sizeof(my_addr));
-	
+    ds_sock = make_tcp_socket(LISTEN_PORT);
     ds_sock_acc = accept(ds_sock, &addr, &addrlen);
     
-	//do something
+	/* do something */
 	
-	close(ds_sock); /* only use of unistd.h */
+	close(ds_sock);
     close(ds_sock_acc);
-    return EXIT_SUCCESS;    /* only use of stdlib.h */
+    return EXIT_SUCCESS;
+}
+
+/* TODO: split more */
+/* creates a TCP socket that listens to all addresses */
+int make_tcp_socket(const u_short sin_port) {
+    int ds_sock;
+    struct sockaddr_in my_addr;
+    
+    ds_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (ds_sock < 0) {
+        fprintf(stderr, "could not create socket: exiting");
+        exit(EXIT_FAILURE);
+    }
+    
+	my_addr.sin_family      = AF_INET;
+	my_addr.sin_port        = htons(sin_port);
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	bind(ds_sock, (struct sockaddr *) &my_addr, sizeof(my_addr));
+    
+    /* TODO: handle bind error as above */
+
+    return ds_sock;
 }
